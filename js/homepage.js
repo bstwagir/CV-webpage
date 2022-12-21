2a588a458111919a29260200009a102de6aa2e12
bstwagir <bstwagir@mtu.edu>
2022-12-21 20:26:48 +0200

    fix-css-html

--- js/homepage.js
+++ js/homepage.js
@@ -2,14 +2,14 @@
 
 document.getElementById('cl').addEventListener('click', toggle_visibility);
 
-function toggle_visibility() {
+/**function toggle_visibility() {
 
     var e =  document.getElementById('head')
     if(e.style.border-color =='black'){ 
 
-        e.style.border-color = 'blue';}
+     //   e.style.border-color = 'blue';}
 
       if(e.style.border-color =='blue'){
-      e.style.border-color ='black';
+//e.style.border-color ='black';
   }
-}
+}**/
